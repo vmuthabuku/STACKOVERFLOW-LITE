f467bbeeae813f99c383439b9ac59cb3a52e5ea0
vmuthabuku <vmuthabuku@gmail.com>
2018-08-18 00:31:06 +0300

    "python flask code that creates endpoints" [finishes #159750998]

---
+++ API/models.py
@@ -0,0 +1,12 @@
+class Question(object):
+    """This class represents the Questions in StackOverflow-lite."""
+
+    def __init__(self, question):
+        self.question = question
+
+
+class Answer(object):
+    """This class represents the Answers in StackOverflow."""
+
+    def __init__(self, answer):
+        self.answer = answer
