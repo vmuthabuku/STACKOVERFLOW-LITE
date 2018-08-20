c1b161debcb0ccbc1412c3172d8c5a3ad6093d23
vmuthabuku <vmuthabuku@gmail.com>
2018-08-20 11:12:32 +0300

    deploying to API to heroku [Finishes #159881484]

---
+++ API2/App/test/test_users.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+import json
+
+class StackOverflow_lite_Users(unittest.TestCase):
+    """This class represent Users."""
+    def setUp(self):
+        """Define test variables and initialize."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client()
+        self.users = {'name': 'vincent', 'email': 'arnoldmaengwe@email.com', 'password': 'let me pas'}
+    def test_signup_users(self):
+        response =  self.client.post(
+            '/api/v1/auth/signup',data = json.dumps(self.users), content_type = 'application/json')
+        self.assertEqual(response.status_code,200)        )
+    
+    def test_signin_user(self):
+        """Test to login a registered user."""
+        response = self.client.post(
+            '/api/v1/auth/signin', data=json.dumps(self.users), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+if __name__ == "__main__":
+    unittest.main() 
--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 [![Build Status](https://travis-ci.com/vmuthabuku/STACKOVERFLOW-LITE.svg?branch=myApi)](https://travis-ci.com/vmuthabuku/STACKOVERFLOW-LITE)
 
 
+[![Coverage Status](https://coveralls.io/repos/github/vmuthabuku/STACKOVERFLOW-LITE/badge.svg)](https://coveralls.io/github/vmuthabuku/STACKOVERFLOW-LITE)
+
+
 
